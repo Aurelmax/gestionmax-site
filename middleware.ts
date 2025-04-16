d0611930a1ea009bb4672e3ef77dcf2dc9fb6080
Aurelmax <aurelien.lavayssiere@gmail.com>
2025-04-16 09:42:49 +0200

    Ajout du middleware avec matcher pour redirection propre

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const host = request.headers.get('host') || ''
+
+  if (host === 'gestionmax.fr') {
+    return NextResponse.redirect(`https://www.gestionmax.fr${request.nextUrl.pathname}`, 301)
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/((?!_next|.*\\..*).*)'],
+}
