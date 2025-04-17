d362ef96f410d989b678a62cc6e2e4c1b52c9cb6
Aurelmax <aurelien.lavayssiere@gmail.com>
2025-04-17 16:16:50 +0200

    Fix export statique + désactivation image optimization pour Scalingo

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // ← nécessaire pour Scalingo si tu veux faire du export statique
   images: {
     domains: ['res.cloudinary.com'],
+    unoptimized: true, // ← désactive l’optimisation d’image côté serveur
   },
-}
+  reactStrictMode: true, // ← recommandé
+};
 
-module.exports = nextConfig
-output: 'export'
-
-
-  
+module.exports = nextConfig;
