46c94b20a991452ca98884b69b9e218aad91ed02
Aurelmax <aurelien.lavayssiere@gmail.com>
2025-04-17 16:27:30 +0200

    Configuration Next.js corrigée pour activer l'auth avec NextAuth.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ← nécessaire pour Scalingo si tu veux faire du export statique
   images: {
     domains: ['res.cloudinary.com'],
     unoptimized: true, // ← désactive l’optimisation d’image côté serveur
